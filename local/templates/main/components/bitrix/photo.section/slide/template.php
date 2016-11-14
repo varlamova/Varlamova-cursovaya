028bf27d12a6121feb24a0386a283a7276120f31
Nadezhda Varlamova <varlamvanadia@mail.ru>
2016-11-15 01:59:43 +0300

    Template of slide component has been changed

---
+++ local/templates/main/components/bitrix/photo.section/slide/template.php
@@ -0,0 +1,33 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div id="slide">
+	<div class="slider">
+		<ul class="items">
+			<li><img src="<?=SITE_TEMPLATE_PATH;?>/images/slide-11.jpg" alt="" /></li>
+			<li><img src="<?=SITE_TEMPLATE_PATH;?>/images/slide-22.jpg" alt="" /></li>
+			<li><img src="<?=SITE_TEMPLATE_PATH;?>/images/slide-33.jpg" alt="" /></li>
+			<li><img src="<?=SITE_TEMPLATE_PATH;?>/images/slide-44.jpg" alt="" /></li>
+			<li><img src="<?=SITE_TEMPLATE_PATH;?>/images/slide-55.jpg" alt="" /></li>
+		</ul>
+	</div>
+	<ul class="pags">
+		<li><a href="#"><strong>0</strong>1</a></li>
+		<li><a href="#"><strong>0</strong>2</a></li>
+		<li><a href="#"><strong>0</strong>3</a></li>
+		<li><a href="#"><strong>0</strong>4</a></li>
+		<li><a href="#"><strong>0</strong>5</a></li>
+	</ul>
+</div>
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -89,22 +89,19 @@ Loc::loadMessages(__FILE__);?>
 <!--==============================header=================================-->
 <div class="header">
     <div>
-        <?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"top_menu", 
-	array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "2",
-		"MENU_CACHE_GET_VARS" => array(
+        <?$APPLICATION->IncludeComponent("bitrix:menu", "top_menu", Array(
+	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+		"DELAY" => "N",	// Откладывать выполнение шаблона меню
+		"MAX_LEVEL" => "2",	// Уровень вложенности меню
+		"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
+			0 => "",
 		),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "left",
-		"USE_EXT" => "Y",
-		"COMPONENT_TEMPLATE" => "top_menu"
+		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+		"MENU_CACHE_TYPE" => "A",	// Тип кеширования
+		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+		"ROOT_MENU_TYPE" => "left",	// Тип меню для первого уровня
+		"USE_EXT" => "Y",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
 	),
 	false
 );?><br>
@@ -126,23 +123,63 @@ Loc::loadMessages(__FILE__);?>
                         <?if(!CSite::InDir('/')):?></a><?endif;?>
                 </a></h1>
         </header>
-        <div id="slide">
-            <div class="slider">
-                <ul class="items">
-                    <li><img src="<?=SITE_TEMPLATE_PATH;?>/images/slide-11.jpg" alt="" /></li>
-                    <li><img src="<?=SITE_TEMPLATE_PATH;?>/images/slide-22.jpg" alt="" /></li>
-                    <li><img src="<?=SITE_TEMPLATE_PATH;?>/images/slide-33.jpg" alt="" /></li>
-                    <li><img src="<?=SITE_TEMPLATE_PATH;?>/images/slide-44.jpg" alt="" /></li>
-                    <li><img src="<?=SITE_TEMPLATE_PATH;?>/images/slide-55.jpg" alt="" /></li>
-                </ul>
-            </div>
-            <ul class="pags">
-                <li><a href="#"><strong>0</strong>1</a></li>
-                <li><a href="#"><strong>0</strong>2</a></li>
-                <li><a href="#"><strong>0</strong>3</a></li>
-                <li><a href="#"><strong>0</strong>4</a></li>
-                <li><a href="#"><strong>0</strong>5</a></li>
-            </ul>
-        </div>
+        <?$APPLICATION->IncludeComponent("bitrix:photo.section", "slide", Array(
+	"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
+		"AJAX_MODE" => "N",	// Включить режим AJAX
+		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+		"AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
+		"BROWSER_TITLE" => "-",	// Установить заголовок окна браузера из свойства
+		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+		"CACHE_GROUPS" => "Y",	// Учитывать права доступа
+		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+		"CACHE_TYPE" => "A",	// Тип кеширования
+		"DETAIL_URL" => "",	// URL, ведущий на страницу с содержимым элемента раздела
+		"DISPLAY_BOTTOM_PAGER" => "N",	// Выводить под списком
+		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+		"ELEMENT_SORT_FIELD" => "sort",	// По какому полю сортируем фотографии
+		"ELEMENT_SORT_ORDER" => "asc",	// Порядок сортировки фотографий в разделе
+		"FIELD_CODE" => array(	// Поля
+			0 => "ID",
+			1 => "NAME",
+			2 => "SORT",
+			3 => "PREVIEW_PICTURE",
+			4 => "",
+		),
+		"FILTER_NAME" => "arrFilter",	// Имя массива со значениями фильтра для фильтрации элементов
+		"IBLOCK_ID" => "7",	// Инфоблок
+		"IBLOCK_TYPE" => "content",	// Тип инфоблока
+		"LINE_ELEMENT_COUNT" => "3",	// Количество фотографий, выводимых в одной строке таблицы
+		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+		"META_DESCRIPTION" => "-",	// Установить описание страницы из свойства
+		"META_KEYWORDS" => "-",	// Установить ключевые слова страницы из свойства
+		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
+		"PAGER_TITLE" => "Фотографии",	// Название категорий
+		"PAGE_ELEMENT_COUNT" => "20",	// Количество элементов на странице
+		"PROPERTY_CODE" => array(	// Свойства
+			0 => "URL",
+			1 => "",
+		),
+		"SECTION_CODE" => "",	// Код раздела
+		"SECTION_ID" => $_REQUEST["SECTION_ID"],	// ID раздела
+		"SECTION_URL" => "",	// URL, ведущий на страницу с содержимым раздела
+		"SECTION_USER_FIELDS" => array(	// Свойства раздела
+			0 => "",
+			1 => "",
+		),
+		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+		"SET_STATUS_404" => "N",	// Устанавливать статус 404
+		"SET_TITLE" => "N",	// Устанавливать заголовок страницы
+		"SHOW_404" => "N",	// Показ специальной страницы
+	),
+	false
+);?><br>
+
     </div>
 </div>
