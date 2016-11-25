faaceeb0fe28ec1008e50de9e0fedaac05a26ccc
Nadezhda Varlamova <varlamvanadia@mail.ru>
2016-11-25 18:48:14 +0300

    DAO KPI department has been created

--- local/modules/sladosti.kpi/lang/en/lib/kpidepartment.php
+++ local/modules/sladosti.kpi/lang/en/lib/kpidepartment.php
@@ -1,7 +1,8 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 123
- * Date: 25.11.2016
- * Time: 18:24
- */
+<?
+$MESS["KPI_ENTITY_ID_FIELD"] = "ID KPI";
+$MESS["KPI_ENTITY_UF_VALUE_FIELD"] = "The value of the index";
+$MESS["KPI_ENTITY_UF_CREATED_FIELD"] = "Date entered values
+index";
+$MESS["KPI_ENTITY_UF_CHANGED_FIELD"] = "Last modified
+the value of the indicator";
+?>
--- local/modules/sladosti.kpi/lang/ru/lib/kpidepartment.php
+++ local/modules/sladosti.kpi/lang/ru/lib/kpidepartment.php
@@ -1,7 +1,8 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 123
- * Date: 25.11.2016
- * Time: 18:24
- */
+<?
+$MESS["KPI_ENTITY_ID_FIELD"] = "ID KPI";
+$MESS["KPI_ENTITY_UF_VALUE_FIELD"] = "Значение показателя";
+$MESS["KPI_ENTITY_UF_CREATED_FIELD"] = "Дата внесения значения
+показателя";
+$MESS["KPI_ENTITY_UF_CHANGED_FIELD"] = "Дата последнего изменения
+значения показателя";
+?>
